5dfb9b95f33adee2c601f0fc7d053ea6de65f472
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-08-02 19:48:42 +0530

    Working on

---
+++ Choose_Your_Adventure/main.py
@@ -0,0 +1,6 @@
+print("**********************************************")
+print("Welcome to Your_Adventure Game")
+print("**********************************************")
+
+username = input("Enter your name: ")
+print(f"Hello {username} , Lets begin our Adventure")
