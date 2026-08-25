6b40bf26e7c781e31927984771a234b0bff07bbe
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-08-25 19:05:29 +0530

    random modules

--- PIG/main.py
+++ PIG/main.py
@@ -1,1 +1,11 @@
 import random
+
+def roll():
+    min_value = 1
+    max_value = 6
+    dice = random.randint(min_value,max_value)
+
+    return roll
+
+
+players = int(input("Enter the number of players (2 - 4): "))
